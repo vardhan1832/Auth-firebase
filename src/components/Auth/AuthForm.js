c48d9521d87b0b6e7a00a8b6e3016361cede07cb
vardhan1831 <sreevardhan259@gmail.com>
2024-01-08 22:13:56 +0530

    Use environment variables for API key

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,75 +1,87 @@
-import { useState, useRef } from 'react';
-
-import classes from './AuthForm.module.css';
+import React,{ useState, useRef } from "react";
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
-  const [isLoading,setIsLoading] = useState(false)
-  const emailInputref = useRef()
-  const passwordInputref = useRef()
+  const [isLoading, setIsLoading] = useState(false);
+  const emailInputref = useRef();
+  const passwordInputref = useRef();
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
-  const submitHandler =async (event) =>{
-    setIsLoading(true)
-    event.preventDefault()
-    const enteredEmail = emailInputref.current.value;
-    const enteredPassword = passwordInputref.current.value;
-
-    if(isLogin){
-
-    }else{
-      const res = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDPpCS5fLqAZ2TYWYHrfIIg4fNr7WQDS-Y',{
-        method:'POST',
-        body:JSON.stringify({
-          email:enteredEmail,
-          password:enteredPassword,
-          returnSecureToken:true
-        }),
-        headers:{
-          'Content-Type':'application/json'
-        }
-      })
-      if(res.ok){
-
-      }else{
-        const data = await res.json()
-        alert(data.error.message)
-        console.log(data)
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+    try {
+      const enteredEmail = emailInputref.current.value;
+      const enteredPassword = passwordInputref.current.value;
+      let url;
+      if (isLogin) {
+        url =
+          `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.AUTHKEY}`;
+      } else {
+        url =
+          `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.AUTHKEY}`;
       }
-      setIsLoading(false)
+      const res = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      if (res.ok) {
+        console.log(data)
+      } else {
+        throw new Error(data.error.message)
+      }
+    } catch (err) {
+      alert(err.message);
     }
-
-  }
+    setIsLoading(false);
+  };
 
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form onSubmit={submitHandler} >
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required ref={emailInputref}/>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputref} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
+          <label htmlFor="password">Your Password</label>
           <input
-            type='password'
-            id='password'
+            type="password"
+            id="password"
             required
             ref={passwordInputref}
           />
         </div>
         <div className={classes.actions}>
-          {!isLoading && <button type='submit'>{isLogin ? 'Login': 'Create Account'}</button>}
-          {isLoading && <p style={{color:'white'}}>{isLogin?'Logging In...':'Creating User'}</p>}
+          {!isLoading && (
+            <button type="submit">
+              {isLogin ? "Login" : "Create Account"}
+            </button>
+          )}
+          {isLoading && (
+            <p style={{ color: "white" }}>
+              {isLogin ? "Logging In..." : "Creating User"}
+            </p>
+          )}
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
